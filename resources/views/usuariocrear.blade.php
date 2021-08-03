22165da5d92ad9e7f348c272e06652dbf15786ea
jcac1808 <jcariascruz.jcac@gmail.com>
2021-08-03 17:28:27 -0400

    Boostrap card

--- resources/views/usuariocrear.blade.php
+++ resources/views/usuariocrear.blade.php
@@ -2,52 +2,53 @@
 
 @section('content')
 
-<h2>Crear Usuario</h2>
+<div class="card">
+  <div class="card-header"><h2>Crear Usuario</h2></div>
+  <div class="card-body">
+    @if ($errors->any())
+    <div class="alert alert-danger">
+      <ul>
+        @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+      </ul>
+    </div>
+    @endif
 
-@if ($errors->any())
-<div class="alert alert-danger">
-  <ul>
-    @foreach ($errors->all() as $error)
-    <li>{{ $error }}</li>
-    @endforeach
-  </ul>
+    <form action="{{ url('usuario/crear') }}" method="POST">
+      {!! csrf_field() !!}
+      <div class="form-group">
+        <label for="profession_id">Id profesion</label>
+        <input type="number" name="profession_id" id="profession_id" class="form-control" value="{{ old('profession_id') }}">
+        @if ($errors->has('profession_id'))
+          <small class="form-text text-danger" >{{ $errors->first('profession_id') }}</small>
+        @endif
+      </div>
+
+      <div class="form-group">
+        <label for="name">Nombre</label>
+        <input type="text" name="name" id="name" class="form-control" value="{{ old('name') }}">
+        @if ($errors->has('name'))
+          <small class="form-text text-danger">{{ $errors->first('name') }}</small>
+        @endif
+      </div>
+
+      <div class="form-group">
+        <label for="email">Correo electronico</label>
+        <input type="email" name="email" id="email" class="form-control" value="{{ old('email') }}">
+        @if ($errors->has('email'))
+          <small class="form-text text-danger">{{ $errors->first('email') }}</small>
+        @endif
+      </div>
+      <div class="form-group">
+        <label for="pwd">Contraseña</label>
+        <input type="password" name="pwd" id="pwd" class="form-control">
+      </div>
+      <button class="btn btn-primary" >Crear usuario </button>
+      <a href="{{ route('home') }}" class="btn btn-primary" >Volver</a>
+    </form>
+  </div>
 </div>
-@endif
-
-<form action="{{ url('usuario/crear') }}" method="POST">
-  {!! csrf_field() !!}
-  <div class="form-group">
-    <label for="profession_id">Id profesion</label>
-    <input type="number" name="profession_id" id="profession_id" class="form-control" value="{{ old('profession_id') }}">
-    @if ($errors->has('profession_id'))
-      <small class="form-text text-danger" >{{ $errors->first('profession_id') }}</small>
-    @endif
-  </div>
-
-  <div class="form-group">
-    <label for="name">Nombre</label>
-    <input type="text" name="name" id="name" class="form-control" value="{{ old('name') }}">
-    @if ($errors->has('name'))
-      <small class="form-text text-danger">{{ $errors->first('name') }}</small>
-    @endif
-  </div>
-
-  <div class="form-group">
-    <label for="email">Correo electronico</label>
-    <input type="email" name="email" id="email" class="form-control" value="{{ old('email') }}">
-    @if ($errors->has('email'))
-      <small class="form-text text-danger">{{ $errors->first('email') }}</small>
-    @endif
-  </div>
-  <div class="form-group">
-    <label for="pwd">Contraseña</label>
-    <input type="password" name="pwd" id="pwd" class="form-control">
-  </div>
-  <button class="btn btn-primary" >Crear usuario </button>
-  <a href="{{ route('home') }}" class="btn btn-primary" >Volver</a>
-</form>
 
 {{-- <a href="{{ url('/') }}">Volver</a> --}}
-
-
 @endsection
--- resources/views/usuarioeditar.blade.php
+++ resources/views/usuarioeditar.blade.php
@@ -1,43 +1,47 @@
 @extends('layout')
 
 @section('content')
-<h2>Editar usuario</h2>
-
-<form action="{{ url('usuario/'.$user->id) }}" method="POST" >
-    {{ method_field('PUT') }}
-    {!! csrf_field() !!}
-    <div class="form-group">
-        <label for="profession_id">Id profesion</label>
-        <input type="number" name="profession_id" id="profession_id" class="form-control" value="{{ old('profession_id', $user->profession_id) }}">    
-        @if ($errors->has('profession_id'))
-            <small class="form-text text-danger">{{ $errors->first('profession_id') }}</small>
-        @endif
+<div class="card">
+    <div class="card-header"><h2>Editar usuario</h2></div>
+    <div class="card-body">
+        <form action="{{ url('usuario/'.$user->id) }}" method="POST" >
+            {{ method_field('PUT') }}
+            {!! csrf_field() !!}
+            <div class="form-group">
+                <label for="profession_id">Id profesion</label>
+                <input type="number" name="profession_id" id="profession_id" class="form-control" value="{{ old('profession_id', $user->profession_id) }}">    
+                @if ($errors->has('profession_id'))
+                    <small class="form-text text-danger">{{ $errors->first('profession_id') }}</small>
+                @endif
+            </div>
+        
+            <div class="form-group">
+                <label for="name">Nombre</label>
+                <input type="text" name="name" id="name" class="form-control" value="{{ old('name', $user->name) }}">
+                @if ($errors->has('name'))
+                    <small class="form-text text-danger">{{ $errors->first('name') }}</small>
+                @endif
+            </div>
+        
+            <div class="form-group">
+                <label for="email">Correo electronico</label>
+                <input type="email" name="email" id="email" class="form-control" value="{{ old('email', $user->email) }}">
+                @if ($errors->has('email'))
+                    <small class="form-text text-danger" >{{ $errors->first('email') }}</small>
+                @endif
+            </div>
+        
+            <div class="form-group">
+                <label for="pwd">Contraseña</label>
+                <input type="password" name="pwd" id="pwd" class="form-control">
+            </div>
+            
+            <button class="btn btn-primary">Actualizar usuario  </button>
+            <a href="{{ route('home') }}" class="btn btn-primary" >Volver</a>
+        </form>
     </div>
+</div>
 
-    <div class="form-group">
-        <label for="name">Nombre</label>
-        <input type="text" name="name" id="name" class="form-control" value="{{ old('name', $user->name) }}">
-        @if ($errors->has('name'))
-            <small class="form-text text-danger">{{ $errors->first('name') }}</small>
-        @endif
-    </div>
-
-    <div class="form-group">
-        <label for="email">Correo electronico</label>
-        <input type="email" name="email" id="email" class="form-control" value="{{ old('email', $user->email) }}">
-        @if ($errors->has('email'))
-            <small class="form-text text-danger" >{{ $errors->first('email') }}</small>
-        @endif
-    </div>
-
-    <div class="form-group">
-        <label for="pwd">Contraseña</label>
-        <input type="password" name="pwd" id="pwd" class="form-control">
-    </div>
-    
-    <button class="btn btn-primary">Actualizar usuario  </button>
-    <a href="{{ route('home') }}" class="btn btn-primary" >Volver</a>
-</form>
 {{-- {{ $user->id }}
 {{ $user->profession_id }}
 {{ $user->name }}
